dd63ec19820702ee58f48ec2eab8a8ddafefc80e
sebastian-escobar <sebastianescobar_92@hotmail.com>
2023-02-27 16:17:49 -0300

    practica js clase 25 de octubre

---
+++ app.js
@@ -0,0 +1,50 @@
+let estado=("perro");
+let tano=14;
+let tito=false;
+
+console.log(estado);
+console.log(tano);
+console.log(tito);
+
+
+//operadores unitarios 
+// operador ++ (suma de a 1)
+let numero = 5;
+numero ++
+numero ++
+numero ++
+console.log(numero);
+
+//operador ++ (resta de a 1)
+let restas = 8;
+restas --
+restas --
+restas --
+console.log(restas);
+
+//operador negar (!)(muestas lo contrario)
+let num = 18;
+console.log (num);
+console.log (!num);
+
+//operadores boleanos (verdadero o falso)
+console.log (1==1); //comparacion (==)
+console.log (1!=1); // diferente  (!=)
+console.log (1<2);  // menor que (<)
+console.log (1>2);  //mayor que (>)
+console.log (1==="1"); // estrictamente igual(===), compara los tipos de datos.
+console.log (1!==2); // estrictamente diferente (!==).
+console.log (1<=1); // menor o igual que (<=).
+console.log (1>=2); // mayor o igual que (>=).
+
+//operadores logicos 
+//         AND (Y)
+console.log (1==1 && 2==2); //true / las dos condiciones se tienen que cumplir como verdaderas.
+console.log (1<1 && 2<1); //false / no se cumplen las condiciones o 1 de las condiciones. 
+
+// OR (o)
+console.log (1<1 || 2==2); //true una de las condiciones se cumple o se cumple las dos. 
+console.log (1<1 || 2<2); // false si las dos condiciones no se cumplen. 
+// NOT
+console.log (`este numero es ${!(5==5)}`); //usando operador de negacion.
+console.log (`este numero es ${(5==5)}`);  // sin usar el operador de negacion. 
