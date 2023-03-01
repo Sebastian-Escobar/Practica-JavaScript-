e226f7cf0a46facf2e826c6b28e054e6bf00f02a
sebastian-escobar <sebastianescobar_92@hotmail.com>
2023-03-01 19:51:46 -0300

    practica del dia

---
+++ 01 de noviembre 2022/1-noviembre.js
@@ -0,0 +1,75 @@
+///Ejercicios javascript Prácticas con variables////
+/*1.- Escribe un programa de una sola línea que haga que aparezca 
+en la pantalla un alert que diga “un mensaje”.
+*/
+ 
+//alert("hola mundo");
+
+/*2.- Escribe un programa de una sola línea que 
+escriba en la pantalla un texto que diga «Hello World» (document.write).
+*/
+//document.write("hello world");
+
+/*3.- Escribe un programa de una sola línea que 
+escriba en la pantalla el resultado de sumar 3 + 5.
+*/
+//document.write(3+5);
+
+/*4.- Escribe un programa de dos líneas que pida el nombre del usuario
+ con un prompt y escriba un texto que diga «Hola nombreUsuario»
+ */
+
+/*let nombre = prompt("nombre del usuario");
+alert(`hola ${nombre}`);
+*/
+
+/*5.- Escribe un programa de tres líneas que pida un 
+número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+*/
+/*let num1= parseInt(prompt("ingresa el primer numero"));
+let num2= parseInt(prompt("ingresa el segundo numero"));
+document.write (num1+num2);
+*/
+
+//6.- Escribe un programa que pida dos números y 
+//escriba en la pantalla cual es el mayor.
+
+/*let num1 = parseInt(prompt("ingresa el primer numero"));
+let num2 = parseInt(prompt("ingresa el segundo numero"));
+if (num1 > num2) {
+    document.write(`el ${num1} es mayor`);
+} else if (num2 > num1) {
+    document.write(`el ${num2} es mayor`);
+} else {
+    document.write("son iguales");
+}
+*/
+/*7.- Escribe un programa que pida 3 números 
+y escriba en la pantalla el mayor de los tres.*/
+/*
+let num1 = parseInt(prompt("ingresa el primer numero"));
+let num2 = parseInt(prompt("ingresa el segundo numero"));
+let num3 = parseInt(prompt("ingresa el segundo numero"));
+
+if (num1 > num2 && num1 > num3) {
+    alert(`el numero ${num1} en mayor`);
+} else if (num2>num3) {
+    alert(`el numero ${num2} es mayor`);
+}else{
+    alert (`el numero ${num3} es mayor`);    
+};*/
+
+/*8.- Escribe un programa que pida un número y diga si es divisible por 2*/
+
+/*let num = parseInt(prompt("ingresa el primer numero"));
+if (num % 2 == 0) {
+    alert(`el numero ${num} es divisible por 2`);
+} else {
+    alert(`el numero ${num} no es divisible por 2`);
+};
+*/
+
+
+
+
