53d89b7efd868e53136c2e6b7ebe8399697a6080
sebastian-escobar <sebastianescobar_92@hotmail.com>
2023-03-03 15:48:21 -0300

    terminano el repaso del dia 03 de noviembre

--- 03 de noviembre 2022/app.js
+++ 03 de noviembre 2022/app.js
@@ -1,6 +1,6 @@
 ///practica array------array=formacion///
 ///creando array///
-let alumnos = [`lili`, `mauro`, 12, "diana", "chicho" ];
+///let alumnos = [`lili`, `mauro`, 12, "diana", "chicho" ];
 //console.log(alumnos);
 ////el ultimo de la lista///
 //console.log (alumnos[ alumnos.length -1]); ////longitud - uno.
@@ -11,14 +11,56 @@ let alumnos = [`lili`, `mauro`, 12, "diana", "chicho" ];
  }
 };*/
 
-//////////----------añadir items a un array (al final) (push)------------//////
-alumnos.push(`maru`,`code`);
-console.log (alumnos);
+//////////----------añadir items a un array (al final) (push)-- (push = empujar)----------//////
+//alumnos.push(`maru`,`code`);
+//console.log (alumnos);
 
-////----------eliminar el autimo elemento (pop)---------------//////////
-alumnos.pop();
-console.log (alumnos);
+////----------eliminar el ultimo elemento (pop)------(pop = estallido(---------//////////
+//alumnos.pop();
+//console.log (alumnos);
 
-/////////........añadir elemento al pricipio (unshift)----------///////////
-alumnos.unshift(`lenovo`);
-console.log (alumnos);
+/////////........añadir elemento al pricipio (unshift)--(unshift = desarmar)--------///////////
+//alumnos.unshift(`lenovo`);
+//console.log (alumnos);
+
+////////-------------- eliminar el primer elemento (shift)-----(shift = cambiar)---------------////
+
+//alumnos.shift();
+//console.log(alumnos);
+
+///////--------------encontrar el indice de un alumno con su nombre (indexOf)-----///////
+///console.log(alumnos.indexOf("chicho")); 
+
+///////-----------eliminar un elemento en particular.....(splice)---------------////////
+
+///alumnos.splice(0,1);  /////(0---> posicion) ----- (1 ----> apartir de esa pocision se elmina).----///
+///console.log(alumnos);
+
+////----añadir un nuevo elemento con splice en una posicion puntual-----////
+//alumnos.splice(2,0,"mariano");
+///console.log(alumnos);
+
+////---concatenar 2 array--- usando (concat)------/////
+let alumnos = [`lili`, `mauro`, 12, "diana", "chicho" ];
+let alumnos2 = [300, 5000, 200];
+
+const alumnos3 = alumnos.concat(alumnos2);
+console.log(alumnos3);
+
+//// determinar la existencia de alguna elemento del array-- con (incluides)---///
+console.log(alumnos.includes("mauro"));
+
+////-------------------inverti el orden de un array-----(reverse)----//////
+console.log(alumnos3.reverse());
+
+////////----------ordenar items en un array---(sort)-(sort = clasificar)---/////
+console.log(alumnos3.sort())
+
+//////-----------estraer sierta parte de un array---(slice)----- (slice = rebanada)-////
+
+/*const alumnos4 = alumnos3.slice(1,2);
+console.log(alumnos4);
+const alumnos4 = alumnos3.slice(1,2);
+console.log(alumnos4);
+const alumnos4 = alumnos3.slice(1,2);
+console.log(alumnos4);*/
