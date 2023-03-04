eb8e4e78a9bcd7a885a0000536e0a4b46d9ff73f
sebastian-escobar <sebastianescobar_92@hotmail.com>
2023-03-03 23:06:11 -0300

    repaso clase 15/11

---
+++ 15 de noviembre 2022/app.js
@@ -0,0 +1,65 @@
+
+//// funcion declarativa////
+/*
+function usuario (name) {
+   console.log(`gracias ${name}`);
+}
+
+function sumar (a, b) {
+    return a + b;
+};
+
+const result = sumar(2,6);
+
+
+function usuario (name = `usuario`) {     ////agregar usuario, es un valor por defecto///
+    console.log(`gracias ${name}`);       ////si no se define un name, x defecto sera usuario///
+ }
+
+ ///funcion anonima////
+
+const name2 = function (name = `Usuario`) {
+    console.log(`gracias ${name}`);   
+};
+
+////funciones flechas///
+
+const suma3 = (a, b) => {
+   return a + b;
+};
+
+////////funcion flecha en una sola linea////
+
+const suma4 = (a, b) => a + b;
+
+*/
+
+/////funcion map/////
+/*
+const alumnos = [`jorge`, `ivan`, `luis`, `rodo`];
+
+alumnos.map((alumno) => console.log (alumno));
+*/
+
+///////operador ternario//////
+/*
+const age = 18;
+
+age >= 18 ? console.log (`es mayor`) : console.log (`es menor`);
+
+const edad = 24 ;
+
+edad >= 25 ? console.log(`es mayor`) : console.log(`es menor`);
+
+const a = 5;
+
+a > 5 ? console.log(`es mayor`) : console.log(`es menor`);
+
+const b = 8;
+
+b>= 8 ? console.log(`es mayor`) : console.log(`es menor`);
+
+const resul = b >= 8 ? `verdadero` : `falso`;
+console.log(resul);
+*/
+ 
