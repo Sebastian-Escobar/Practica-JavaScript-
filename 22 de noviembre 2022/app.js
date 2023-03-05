7f6bd0492aca47d9264e28475888672cfecad746
sebastian-escobar <sebastianescobar_92@hotmail.com>
2023-03-05 20:14:57 -0300

    repaso

---
+++ 22 de noviembre 2022/app.js
@@ -0,0 +1,37 @@
+/////objetos////
+const auto = {
+    color: `rojo`,
+    marca: `fiat`,
+    modelo: `2020`,
+    autonomia: false,
+    combustible: [`gasoil`, `nafta`, `electrico`]
+};
+/*
+console.log(auto);
+
+///modificar valor////
+
+auto.color = `amarilo`;
+
+////agregar key al objeto///
+
+auto.puertas = 5;
+ 
+/// eliminar key //// key/value= a propiedad////   Delete ===> eliminar
+
+delete auto.marca;
+
+console.log (auto); 
+
+////recorrer un objeto////
+///valores de una keys//// (propiedades).
+
+console.log (Object.keys(auto));
+
+const llaves = Object.keys(auto);
+for (let i = 0; i < llaves.length; i++) {
+    let llave = llaves[i];
+    console.log(auto[llave]); 
+}
+ 
+*/
